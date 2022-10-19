c3e0b8e0225fb4692a233d93a7696e5652e44ebd
Fandi Sun <115258420+Fandi-Sun@users.noreply.github.com>
2022-10-19 15:07:48 +0100

    Update DFT.m

--- AdaptiveMlstein/1D/DFT.m
+++ AdaptiveMlstein/1D/DFT.m
@@ -1,7 +1,7 @@
 function outcome=DFT(nonlinearity,u)
 % drift function
 % Notice: if change the drift here, 
-%       the TSRK method in 'main_1D.m'
+%       the TSRK method in 'main1D.m'
 %               will need to be updated accordingly 
 %       and the paramter parms.alpha 
 %               might need to be updated
