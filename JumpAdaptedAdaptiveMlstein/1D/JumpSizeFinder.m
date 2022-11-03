c951eb4557ca9870f218b09ecff729bccd2b4844
Fandi Sun <115258420+Fandi-Sun@users.noreply.github.com>
2022-11-03 22:40:41 +0000

    Update JumpSizeFinder.m

--- JumpAdaptedAdaptiveMlstein/1D/JumpSizeFinder.m
+++ JumpAdaptedAdaptiveMlstein/1D/JumpSizeFinder.m
@@ -5,13 +5,13 @@ function [dt, jumpIndct, JumpSize, JumpTimesTemp, zetaTemp]=...
 % % 't' -- (scalar) starting time of this step
 % % 'dt' -- (scalar) step size
 % % 'JumpTimesTemp' -- (1-by-n vector)  jump times in this MC realisation
-% % 'zetaTemp' -- (1-by-n vector)  jump size in this MC realisation 
+% % 'zetaTemp' -- (m-by-n matrix)  jump size in this MC realisation 
 % % OUTPUTS:
 % % 'dt' -- (scalar) the step size to use if there is a jump
 % % 'jumpIndct' -- (scalar) indicator of whether there is a jump
 % % 'JumpTimesTemp' -- (1-by-(n-1) vector)  
 % %         updated jump times in this MC realisation  
-% % 'zetaTemp' -- (1-by-(n-1) vector)  
+% % 'zetaTemp' -- (m-by-(n-1) matrix)  
 % %         updated jump size in this MC realisation 
 
 % the following if statement works for both fixed step method
@@ -21,10 +21,10 @@ function [dt, jumpIndct, JumpSize, JumpTimesTemp, zetaTemp]=...
 if JumpTimesTemp(1) <= t+dt
     % update 'dt' if 'JumpTimesTemp(1) <= t+dt'
     dt=JumpTimesTemp(1) - t;
-    JumpSize=zetaTemp(1); % jump size
+    JumpSize=zetaTemp(:,1); % jump size
     % removing the first checked element 
     JumpTimesTemp=JumpTimesTemp(2:end);  
-    zetaTemp=zetaTemp(2:end);  
+    zetaTemp=zetaTemp(:,2:end);  
     % indicator of whether this step has a jump
     jumpIndct=1;  
 else  % if there is no jump in this step
